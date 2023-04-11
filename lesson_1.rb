a2cf8cafa3cf2334a55cbcedae71cb8352c5c197
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-11 11:20:02 -0700

    RB 130 Lesson 1

---
+++ lesson_1.rb
@@ -0,0 +1,40 @@
+##### Rb 130 Lesson 1: Blocks #####
+
+=begin
+# Blocks
+
+[1,2,3].each do |n|
+  puts n
+end
+
+# or
+
+[1,2,3].each {|x| puts x}
+
+# block syntax using `do...end` or `{...}`
+
+# Passing in a block to `Integer#times` method.
+
+3.times do |num|
+  puts num
+end
+
+#=> 3 (Returns the calling object 3, outputs 0,1,2)
+
+# Passing a block to the `Array#map` method.
+
+[1,2,3].map do |num|
+  num + 1
+end
+
+#=> Returns a new array of the manipulated values from the block, [2,3,4]
+
+# Passing in a block to the `Hash#select` method.
+
+{ a: 1, b: 2, c: 3, d: 4, e: 5 }.select do |_, value|
+  value > 2
+end
+
+#=> Returns a new hash object according the the return value of the block. { c: 3, d: 4, e: 5 }
+
+=end
