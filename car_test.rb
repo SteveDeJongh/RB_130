b9103bd0e367cd21692e855ad8d88a287f862401
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-15 15:39:12 -0700

    RB 130 Lesson 2

--- car_test.rb
+++ car_test.rb
@@ -1,5 +1,5 @@
 #### Lesson 4: car test ####
-
+=begin
 require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use!
@@ -47,5 +47,48 @@ class CarTest < MiniTest::Test
     arr << car
   
     assert_includes(arr, car)
+  end
+end
+
+=end
+
+# Assertion style syntax, with a setup step.
+
+require 'minitest/autorun'
+
+require_relative 'car'
+
+class CarTest < MiniTest::Test
+  def setup
+    @car = Car.new
+  end
+
+  def test_car_exists
+    assert(@car)
+  end
+
+  def test_wheels
+    assert_equal(4, @car.wheels)
+  end
+
+  def test_name_is_nil
+    assert_nil(@car.name)
+  end
+
+  def test_raise_initialize_with_arg
+    assert_raises(ArgumentError) do
+      Car.new(name: "Joey")
+    end
+  end
+
+  def test_instance_of_car
+    assert_instance_of(Car, @car)
+  end
+
+  def test_includes_car
+    arr = [1, 2, 3]
+    arr << @car
+
+    assert_includes(arr, @car)
   end
 end
