bd312be3fd3e3bf823c3faa71e65a553f0c2f0f8
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-15 14:34:53 -0700

    RB 130 Lesson 2

--- car_test.rb
+++ car_test.rb
@@ -1,6 +1,8 @@
 #### Lesson 4: car test ####
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 require_relative 'car'
 
@@ -8,5 +10,11 @@ class CarTest < MiniTest::Test
   def test_wheels
     car = Car.new
     assert_equal(4, car.wheels)
+  end
+
+  def test_bad_wheels
+    skip "Skipping a test" # Messages can be added to `skip` keyword to provide more detail.
+    car = Car.new
+    assert_equal(3, car.wheels)
   end
 end
