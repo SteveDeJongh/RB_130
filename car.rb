f60c6917f946378ef0de06ece873345ee9e1877e
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-15 13:46:43 -0700

    RB 130 Lesson 2

---
+++ car.rb
@@ -0,0 +1,9 @@
+#### Testing Lesson 2: First test ####
+
+class Car
+  attr_accessor :wheels
+
+  def initialize
+    @wheels = 4
+  end
+end
---
+++ car_test.rb
@@ -0,0 +1,12 @@
+#### Lesson 4: car test ####
+
+require 'minitest/autorun'
+
+require_relative 'car'
+
+class CarTest < MiniTest::Test
+  def test_wheels
+    car = Car.new
+    assert_equal(4, car.wheels)
+  end
+end
