46e9ddcdba1c1db24501f7266b1555e85ccc055d
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-16 14:56:09 -0700

    RB 130 Lesson 2 small problems

--- rb_130_practice_problems/easy_1.rb
+++ rb_130_practice_problems/easy_1.rb
@@ -266,7 +266,29 @@ p one?([1, 3, 5, 7]) { |value| true }           # -> false
 p one?([1, 3, 5, 7]) { |value| false }          # -> false
 p one?([]) { |value| true }                     # -> false
 
-=end
-
 # 10) Count Items
 
+def count(arr)
+  count = 0
+  arr.each {|x| count += 1 if yield(x)}
+  count
+end
+
+# FE, find a solution without using each, loop, while, or until.
+
+def count(arr)
+  total = 0
+  0.upto(arr.size - 1) do |index|
+    total += 1 if yield(arr[index])
+  end
+  total
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
+
+=end
