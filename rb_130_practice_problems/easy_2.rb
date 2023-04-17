94b33826e7d062579556b4b0695806d86d80214e
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-16 17:45:11 -0700

    RB 130 Lesson 2 small problems

--- rb_130_practice_problems/easy_2.rb
+++ rb_130_practice_problems/easy_2.rb
@@ -128,7 +128,148 @@ end
 
 puts result == [1, 3, 6]
 
-=end
-
 # 7) each_with_object
 
+def each_with_object(col, obj)
+  col.each {|item| yield(item, obj)}
+  obj
+end
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << value**2
+end
+p result == [1, 9, 25]
+
+result = each_with_object([1, 3, 5], []) do |value, list|
+  list << (1..value).to_a
+end
+p result == [[1], [1, 2, 3], [1, 2, 3, 4, 5]]
+
+result = each_with_object([1, 3, 5], {}) do |value, hash|
+  hash[value] = value**2
+end
+p result == { 1 => 1, 3 => 9, 5 => 25 }
+
+result = each_with_object([], {}) do |value, hash|
+  hash[value] = value * 2
+end
+p result == {}
+
+# 8) max_by
+
+def max_by(col)
+  result = []
+  col.each {|el| result << yield(el)}
+  idx = result.index(result.max)
+  col[idx] unless col.empty?
+end
+
+# Or
+
+def max_by(array)
+  return nil if array.empty?
+
+  max_element = array.first
+  largest = yield(max_element)
+
+  array[1..-1].each do |item|
+    yielded_value = yield(item)
+    if largest < yielded_value
+      largest = yielded_value
+      max_element = item
+    end
+  end
+
+  max_element
+end
+
+p max_by([1, 5, 3]) { |value| value + 2 } == 5
+p max_by([1, 5, 3]) { |value| 9 - value } == 1
+p max_by([1, 5, 3]) { |value| (96 - value).chr } == 1
+p max_by([[1, 2], [3, 4, 5], [6]]) { |value| value.size } == [3, 4, 5]
+p max_by([-7]) { |value| value * 3 } == -7
+p max_by([]) { |value| value + 5 } == nil
+
+# 9) each_cons (Part1 )
+
+def each_cons(col)
+  times = col.size - 1
+  times.times do |n|
+    yield(col[n], col[n+1])
+  end
+
+  nil
+end
+
+# Or using each_with_index and break
+
+def each_cons(array)
+  array.each_with_index do |item, index|
+    break if index + 1 >= array.size
+    yield(item, array[index + 1])
+  end
+  nil
+end
+
+hash = {}
+result = each_cons([1, 3, 6, 10]) do |value1, value2|
+  hash[value1] = value2
+end
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+result = each_cons([]) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {}
+p result == nil
+
+hash = {}
+result = each_cons(['a', 'b']) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {'a' => 'b'}
+p result == nil
+
+# 10) each_cons (Part 2)
+
+def each_cons(col, num)
+  col.each_index do |index|
+    break if index + num - 1 >= col.size
+    yield(*col[index..(index + num -1)])
+  end
+  nil
+end
+
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
+
+=end
