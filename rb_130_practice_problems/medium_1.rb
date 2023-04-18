8c9088f130afe4ce0c41d3b9de04e805e127680c
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-17 17:36:41 -0700

    RB 130 Lesson 2 small problems

--- rb_130_practice_problems/medium_1.rb
+++ rb_130_practice_problems/medium_1.rb
@@ -125,8 +125,96 @@ end
 # apples, corn, cabbage, and wheat
 # We've finished gathering!
 
-=end
-
 # 6) Method to Proc
 
-# 7) Buble Sort with Blocks
+# Replace the two `method_name` placeholders with actual method calls
+# def convert_to_base_8(n)
+#   n.method_name.method_name
+# end
+def convert_to_base_8(n)
+  n.to_s(8).to_i
+end
+
+# Replace `argument` with the correct argument below
+# `method` is `Object#method`, not a placeholder
+# base8_proc = method(argument).to_proc
+base8_proc = method(:convert_to_base_8).to_proc # Passing a symbol of the method we want to pass to `Object#method`
+
+# We'll need a Proc object to make this code work
+# Replace `a_proc` with the correct object
+# [8, 10, 12, 14, 16, 33].map(&a_proc)
+p [8, 10, 12, 14, 16, 33].map(&base8_proc) == [10, 12, 14, 16, 20, 41]
+
+# 7) Buble Sort with Blocks # Revisit FE
+
+# def bubble_sort!(array)
+#   if block_given?
+#     loop do
+#       swapped = false
+#       1.upto(array.size - 1) do |index|
+#         next if yield(array[index - 1], array[index])
+#         array[index - 1], array[index] = array[index], array[index - 1]
+#         swapped = true
+#       end
+  
+#       break unless swapped
+#     end
+#   else
+#     loop do
+#       swapped = false
+#       1.upto(array.size - 1) do |index|
+#         next if array[index - 1] <= array[index]
+#         array[index - 1], array[index] = array[index], array[index - 1]
+#         swapped = true
+#       end
+
+#       break unless swapped
+#     end
+#   end
+# end
+
+# Or essentially moving the `if block_given?` query to occur on each iteration of the collection.
+
+def bubble_sort!(array)
+  loop do
+    swapped = false
+    1.upto(array.size - 1) do |index|
+      if block_given?
+        next if yield(array[index - 1], array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
+
+      array[index - 1], array[index] = array[index], array[index - 1]
+      swapped = true
+    end
+
+    break unless swapped
+  end
+end
+
+array = [5, 3]
+bubble_sort!(array)
+p array == [3, 5]
+
+array = [5, 3, 7]
+bubble_sort!(array) { |first, second| first >= second }
+p array == [7, 5, 3]
+
+array = [6, 2, 7, 1, 4]
+bubble_sort!(array)
+p array == [1, 2, 4, 6, 7]
+
+array = [6, 12, 27, 22, 14]
+bubble_sort!(array) { |first, second| (first % 7) <= (second % 7) }
+p array == [14, 22, 12, 6, 27]
+
+array = %w(sue Pete alice Tyler rachel Kim bonnie)
+bubble_sort!(array)
+p array == %w(Kim Pete Tyler alice bonnie rachel sue)
+
+array = %w(sue Pete alice Tyler rachel Kim bonnie)
+bubble_sort!(array) { |first, second| first.downcase <= second.downcase }
+p array == %w(alice bonnie Kim Pete rachel sue Tyler)
+
+=end
