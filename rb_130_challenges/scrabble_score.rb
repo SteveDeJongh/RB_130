3035a1fcd5e74a2f985e198d811fdc5613c8f634
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-23 12:13:01 -0700

    RB 130 Lesson 2 challenges

--- rb_130_challenges/scrabble_score.rb
+++ rb_130_challenges/scrabble_score.rb
@@ -23,7 +23,7 @@ Examples:
 Cabbage = 14
 
 Data:
-Arrays, Hashes, integers, and strings
+Hashes, integers, and strings
 
 Algorithm:
 initialize hash to contain the key as the score for the letter, with the value an array of the letters for that score.
@@ -34,9 +34,6 @@ Create a new "scrabble" class.
  define `score` method
    initialize `score` local variable to 0
    iterate over characters in word
-     
-   
-
 
 Code:
 
@@ -44,8 +41,6 @@ Code:
 
 SCORES = { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C: 3, M: 3, P: 3,
            F: 4, H: 4, V: 4, W: 4, Y: 4, K: 5, J: 8, X: 8, Q: 10, Z: 10}
-
-# p SCORES
 
 class Scrabble
   def initialize(word)
@@ -55,28 +50,45 @@ class Scrabble
   def score
     return 0 if @word == nil
     word = @word.strip.upcase
-    result = 0
-
-    word.chars.each {|char| result += SCORES.fetch(char.to_sym)}
-    result
-
+    word.chars.map {|char| SCORES.fetch(char.to_sym)}.sum
   end
 
   def self.score(input)
-    return 0 if input == nil
-    word = input.upcase
-    result = 0
-
-    word.chars.each {|char| result += SCORES.fetch(char.to_sym)}
-    result
+    Scrabble.new(input.upcase).score
   end
-
-  private
-
-  # attr_reader :word
 end
 
-test1 = Scrabble.new('cabbage')
+# Launch School Solution using REGEX
 
-p test1
-p test1.score
+class Scrabble
+  attr_reader :word
+
+  POINTS = {
+    'AEIOULNRST'=> 1,
+    'DG' => 2,
+    'BCMP' => 3,
+    'FHVWY' => 4,
+    'K' => 5,
+    'JX' => 8,
+    'QZ' => 10
+}
+  def initialize(word)
+    @word = word ? word : ''
+  end
+
+  def score
+    letters = word.upcase.gsub(/[^A-Z]/, '').chars # Upcases, and cleans string of newline/tab characters
+
+    total = 0
+    letters.each do |letter|
+      POINTS.each do |all_letters, point|
+        total += point if all_letters.include?(letter)
+      end
+    end
+    total
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+  end
+end
