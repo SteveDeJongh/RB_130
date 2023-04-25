5bd94bf2ebe8c79d91a98d1e4314cbdabcac080b
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-25 15:03:40 -0700

    RB 130 Lesson 2 challenges

--- rb_130_challenges/beer_song.rb
+++ rb_130_challenges/beer_song.rb
@@ -30,49 +30,34 @@ Last Verse (0 bottles):
 "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
 =end
 
+# First solution, no very OOO style.
+
+=begin
+
 class BeerSong
-  def self.verse(start = 99, finish = start)
+  def self.verse(line)
     result = ""
-    start.downto(finish) do |verse|
-      result += "\n" unless verse == start
-      if verse > 2 
-        # puts "#{verse} bottles of beer on the wall, #{verse} bottles of beer."
-        # puts "Take one down and pass it around, #{verse - 1} bottles of beer on the wall."
-        result += "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\n" +
-        "Take one down and pass it around, #{verse - 1} bottles of beer on the wall.\n"
-      elsif verse == 2
-        result += "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\n" +
-        "Take one down and pass it around, #{verse - 1} bottle of beer on the wall.\n"
-      elsif verse == 1
-        result += "#{verse} bottle of beer on the wall, #{verse} bottle of beer.\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
-      else
-        result += "No more bottles of beer on the wall, no more bottles of beer.\n" + 
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-      end
+    if line > 2 
+      result += "#{line} bottles of beer on the wall, #{line} bottles of beer.\n" +
+      "Take one down and pass it around, #{line - 1} bottles of beer on the wall.\n"
+    elsif line == 2
+      result += "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    elsif line == 1
+      result += "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      result += "No more bottles of beer on the wall, no more bottles of beer.\n" + 
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
     result
   end
 
-  def self.verses(start = 99, finish = start)
+  def self.verses(start, finish)
     result = ""
     start.downto(finish) do |verse|
       result += "\n" unless verse == start
-      if verse > 2 
-        # puts "#{verse} bottles of beer on the wall, #{verse} bottles of beer."
-        # puts "Take one down and pass it around, #{verse - 1} bottles of beer on the wall."
-        result += "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\n" +
-        "Take one down and pass it around, #{verse - 1} bottles of beer on the wall.\n"
-      elsif verse == 2
-        result += "#{verse} bottles of beer on the wall, #{verse} bottles of beer.\n" +
-        "Take one down and pass it around, #{verse - 1} bottle of beer on the wall.\n"
-      elsif verse == 1
-        result += "#{verse} bottle of beer on the wall, #{verse} bottle of beer.\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
-      else
-        result += "No more bottles of beer on the wall, no more bottles of beer.\n" + 
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-      end
+      result += self.verse(verse)
     end
     result
   end
@@ -83,3 +68,71 @@ class BeerSong
 end
 
 p BeerSong.verse(2)
+
+=end
+
+# A more OOO approach.
+
+class Verse
+  attr_reader :bottles
+
+  def initialize(bottles)
+    @bottles = bottles
+  end
+
+  def single_verse
+    case bottles
+    when 0 then zero_bottle_verse
+    when 1 then single_bottle_verse
+    when 2 then two_bottle_verse
+    else        default_verse
+    end
+  end
+
+  private
+
+  def default_verse
+    "#{bottles} bottles of beer on the wall, #{bottles}" +
+    " bottles of beer.\nTake one down and pass it around, " +
+    "#{bottles-1} bottles of beer on the wall.\n"
+  end
+
+  def two_bottle_verse
+    "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+    "Take one down and pass it around, 1 bottle of beer " +
+    "on the wall.\n"
+  end
+
+  def single_bottle_verse
+    "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+    "Take it down and pass it around, no more bottles of beer " +
+    "on the wall.\n"
+  end
+
+  def zero_bottle_verse
+    "No more bottles of beer on the wall, no more bottles " +
+    "of beer.\nGo to the store and buy some more, 99 bottles " +
+    "of beer on the wall.\n"
+  end
+
+end
+
+class BeerSong
+  def self.verse(number)
+    Verse.new(number).single_verse
+  end
+
+  def self.verses(start, stop)
+    result = []
+
+    start.downto(stop) do |line|
+      result << "#{verse(line)}"
+    end
+
+    result.join("\n")
+  end
+
+  def self.lyrics
+    verses(99, 0)  
+  end
+end
