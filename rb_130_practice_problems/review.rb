03222bf217a09f516a4879f6d4af3833db568a14
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-28 15:41:27 -0700

    RB 130 test prep

--- rb_130_practice_problems/review.rb
+++ rb_130_practice_problems/review.rb
@@ -30,3 +30,25 @@ Purpose of core tools
 Gemfiles
 
 =end
+
+# Yielding to a block, and passing in a method parameter to be the block parameter.
+
+def takeablock(method_param)
+  yield(method_param)
+end
+
+takeablock('method_arg') { |block_param| puts block_param } #=> "method_arg"
+
+def time_entry
+  time_before = Time.now
+  yield
+  time_after = Time.now
+
+  puts "It took #{time_after - time_before} seconds."
+end
+
+time_entry do
+  puts "What's your name?"
+  input = gets.chomp
+  puts input
+end 
