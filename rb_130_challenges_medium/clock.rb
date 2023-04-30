06a3c47cce2135960dd0370122fe64f724d210ed
Steve De Jongh <de.jongh.steve@gmail.com>
2023-04-30 14:08:07 -0700

    RB 130 Lesson 3 challenges

--- rb_130_challenges_medium/clock.rb
+++ rb_130_challenges_medium/clock.rb
@@ -21,6 +21,8 @@ instance
 
 =end
 
+# My Solution
+
 class Clock
   attr_accessor :time
 
@@ -42,7 +44,7 @@ class Clock
   end
 
   def -(minutes)
-
+    self.+(-minutes)
   end
 
   def ==(other)
@@ -55,7 +57,60 @@ class Clock
 
 end
 
-cl = Clock.at(10, 30)
-cl2 = Clock.at(10, 30)
-p cl
-p cl + 1660
+# LS Solution
+
+# class Clock
+#   attr_reader :hour, :minute
+
+#   ONE_DAY = 24 * 60
+
+#   def initialize(hour, minute)
+#     @hour = hour
+#     @minute = minute
+#   end
+
+#   def self.at(hour, minute=0)
+#     new(hour, minute)
+#   end
+
+#   def +(add_minutes)
+#     minutes_since_midnight = compute_minutes_since_midnight + add_minutes
+#     while minutes_since_midnight >= ONE_DAY
+#       minutes_since_midnight -= ONE_DAY
+#     end
+
+#     compute_time_from(minutes_since_midnight)
+#   end
+
+#   def -(sub_minutes)
+#     minutes_since_midnight = compute_minutes_since_midnight - sub_minutes
+
+#     while minutes_since_midnight < 0
+#       minutes_since_midnight += ONE_DAY
+#     end
+
+#     compute_time_from(minutes_since_midnight)
+#   end
+
+#   def ==(other_time)
+#     hour == other_time.hour && minute == other_time.minute
+#   end
+
+#   def to_s
+#     format('%02d:%02d', hour, minute);
+#   end
+
+#   private
+
+#   def compute_minutes_since_midnight
+#     total_minutes = 60 * hour + minute
+#     total_minutes % ONE_DAY
+#   end
+
+#   def compute_time_from(minutes_since_midnight)
+#     hours, minutes = minutes_since_midnight.divmod(60)
+#     # hours %= 24
+#     self.class.new(hours, minutes)
+#   end
+# end
+
